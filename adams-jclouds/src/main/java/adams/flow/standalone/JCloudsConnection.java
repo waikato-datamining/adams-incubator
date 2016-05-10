356586cc8f0b6650e1b59645c6c355ac174b52b4
fracpete <fracpete@gmail.com>
2016-05-10 04:19:33 +0000

    removed API class again, as it is very specific to actions

--- adams-jclouds/src/main/java/adams/flow/standalone/JCloudsConnection.java
+++ adams-jclouds/src/main/java/adams/flow/standalone/JCloudsConnection.java
@@ -21,7 +21,6 @@
 package adams.flow.standalone;
 
 import adams.core.QuickInfoHelper;
-import adams.core.base.BaseClassname;
 import adams.core.base.BasePassword;
 import adams.core.base.BaseURL;
 import adams.core.net.JClouds;
@@ -32,6 +31,7 @@ import org.jclouds.ContextBuilder;
 import org.jclouds.logging.slf4j.config.SLF4JLoggingModule;
 
 import java.io.Closeable;
+import java.util.HashMap;
 import java.util.logging.Level;
 
 /**
@@ -96,12 +96,7 @@ import java.util.logging.Level;
  * &nbsp;&nbsp;&nbsp;The URL for the API endpoint.
  * &nbsp;&nbsp;&nbsp;default: https:&#47;&#47;somehost:5000&#47;v2.0
  * </pre>
- * 
- * <pre>-api-class &lt;adams.core.base.BaseClassname&gt; (property: APIClass)
- * &nbsp;&nbsp;&nbsp;The API class for the provider.
- * &nbsp;&nbsp;&nbsp;default: java.lang.Object
- * </pre>
- * 
+ *
  <!-- options-end -->
  *
  * @author  fracpete (fracpete at waikato dot ac dot nz)
@@ -125,11 +120,8 @@ public class JCloudsConnection
   /** the endpoint. */
   protected BaseURL m_Endpoint;
 
-  /** the API class. */
-  protected BaseClassname m_APIClass;
-
-  /** the API instance. */
-  protected Closeable m_APIInstance;
+  /** the API instances. */
+  protected HashMap<Class,Closeable> m_APIInstances;
 
   /**
    * Returns a string describing the object.
@@ -163,10 +155,6 @@ public class JCloudsConnection
     m_OptionManager.add(
       "endpoint", "endpoint",
       JClouds.getEndpoint());
-
-    m_OptionManager.add(
-      "api-class", "APIClass",
-      new BaseClassname(Object.class));
   }
 
   /**
@@ -295,35 +283,6 @@ public class JCloudsConnection
   }
 
   /**
-   * Sets the API class to use.
-   *
-   * @param value	the class
-   */
-  public void setAPIClass(BaseClassname value) {
-    m_APIClass = value;
-    reset();
-  }
-
-  /**
-   * Returns the API class to use.
-   *
-   * @return		the class
-   */
-  public BaseClassname getAPIClass() {
-    return m_APIClass;
-  }
-
-  /**
-   * Returns the tip text for this property.
-   *
-   * @return 		tip text for this property suitable for
-   * 			displaying in the GUI or for listing the options.
-   */
-  public String APIClassTipText() {
-    return "The API class for the provider.";
-  }
-
-  /**
    * Returns a quick info about the actor, which will be displayed in the GUI.
    *
    * @return		null if no info available, otherwise short string
@@ -335,7 +294,6 @@ public class JCloudsConnection
     result  = QuickInfoHelper.toString(this, "provider", m_Provider, "provider: ");
     result += QuickInfoHelper.toString(this, "identity", m_Identity, ", identity: ");
     result += QuickInfoHelper.toString(this, "endpoint", m_Endpoint, ", endpoint: ");
-    result += QuickInfoHelper.toString(this, "apiClass", m_APIClass, ", API: ");
 
     return result;
   }
@@ -363,12 +321,15 @@ public class JCloudsConnection
   /**
    * Builds the API.
    *
+   * @param cls		the API class to use
    * @return		the API
    */
-  public synchronized Closeable buildAPI() {
-    if (m_APIInstance == null)
-      m_APIInstance = getContextBuilder().buildApi(m_APIClass.classValue());
-    return m_APIInstance;
+  public synchronized Closeable buildAPI(Class cls) {
+    if (m_APIInstances == null)
+      m_APIInstances = new HashMap<>();
+    if (!m_APIInstances.containsKey(cls))
+      m_APIInstances.put(cls, getContextBuilder().buildApi(cls));
+    return m_APIInstances.get(cls);
   }
 
   /**
@@ -385,14 +346,16 @@ public class JCloudsConnection
    * Closes the API if necessary.
    */
   protected void closeAPI() {
-    if (m_APIInstance != null) {
-      try {
-	Closeables.close(m_APIInstance, true);
+    if (m_APIInstances != null) {
+      for (Class key: m_APIInstances.keySet()) {
+	try {
+	  Closeables.close(m_APIInstances.get(key), true);
+	}
+	catch (Exception e) {
+	  getLogger().log(Level.WARNING, "Failed to close API for API class: " + key.getName(), e);
+	}
       }
-      catch (Exception e) {
-	getLogger().log(Level.WARNING, "Failed to close API!", e);
-      }
-      m_APIInstance = null;
+      m_APIInstances = null;
     }
   }
 
