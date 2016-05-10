c50ba51234fe1d59e28462a3a1516c417581130d
fracpete <fracpete@gmail.com>
2016-05-10 04:30:11 +0000

    action for listing regions

---
+++ adams-openstack/src/main/java/adams/flow/source/jclouds/ListRegions.java
@@ -0,0 +1,114 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * ListRegions.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.source.jclouds;
+
+import org.jclouds.openstack.nova.v2_0.NovaApi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ <!-- globalinfo-start -->
+ * Lists the configured org.jclouds.openstack.nova.v2_0.NovaApi regions.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class ListRegions
+  extends AbstractJCloudsSourceAction {
+
+  private static final long serialVersionUID = -6630288063048110072L;
+
+  /** the regions. */
+  protected List<String> m_Regions;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Lists the configured " + NovaApi.class.getName() + " regions.";
+  }
+
+  /**
+   * Resets the scheme.
+   */
+  @Override
+  protected void reset() {
+    super.reset();
+    m_Regions = new ArrayList<>();
+  }
+
+  /**
+   * Returns the data types of the generated data.
+   *
+   * @return		the data types
+   */
+  @Override
+  public Class[] generates() {
+    return new Class[]{String[].class};
+  }
+
+  /**
+   * Performs the actual action.
+   *
+   * @return		null if successful, otherwise error message
+   */
+  @Override
+  protected String doExecute() {
+    m_Regions.clear();
+    m_Regions.addAll(((NovaApi) m_Connection.buildAPI(NovaApi.class)).getConfiguredRegions());
+    return null;
+  }
+
+  /**
+   * Returns whether any data was generated.
+   *
+   * @return		true if data available to be collected
+   * @see		#output()
+   */
+  @Override
+  public boolean hasPendingOutput() {
+    return (m_Regions.size() > 0);
+  }
+
+  /**
+   * Returns the generated data.
+   *
+   * @return		the generated data
+   */
+  @Override
+  public Object output() {
+    return m_Regions.toArray(new String[m_Regions.size()]);
+  }
+}
