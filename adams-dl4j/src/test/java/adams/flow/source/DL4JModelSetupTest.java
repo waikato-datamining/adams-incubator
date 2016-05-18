792c727d2cb03e4abf162953f30ab9c8cf072c9e
fracpete <fracpete@gmail.com>
2016-05-18 08:27:22 +0000

    turned it into proper test

--- adams-dl4j/src/test/java/adams/flow/source/DL4JModelSetupTest.java
+++ adams-dl4j/src/test/java/adams/flow/source/DL4JModelSetupTest.java
@@ -20,17 +20,27 @@
 
 package adams.flow.source;
 
+import adams.core.io.PlaceholderFile;
+import adams.core.option.AbstractArgumentOption;
+import adams.data.conversion.DL4JModelToYaml;
 import adams.env.Environment;
 import adams.flow.AbstractFlowTest;
 import adams.flow.control.Flow;
 import adams.flow.core.AbstractActor;
+import adams.flow.core.Actor;
+import adams.flow.execution.NullListener;
+import adams.flow.sink.DumpFile;
+import adams.flow.transformer.Convert;
+import adams.ml.dl4j.model.SimpleMultiLayerNetwork;
+import adams.test.TmpFile;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Test for DL4JModelSetup actor.
- * <br>
- * NB: Dummy test.
  *
  * @author fracpete
  * @author adams.core.option.FlowJUnitTestProducer (code generator)
@@ -49,6 +59,38 @@ public class DL4JModelSetupTest
   }
 
   /**
+   * Called by JUnit before each test method.
+   *
+   * @throws Exception 	if an error occurs.
+   */
+  protected void setUp() throws Exception {
+    super.setUp();
+    
+    m_TestHelper.deleteFileFromTmp("dumpfile.txt");
+  }
+
+  /**
+   * Called by JUnit after each test method.
+   *
+   * @throws Exception	if tear-down fails
+   */
+  protected void tearDown() throws Exception {
+    m_TestHelper.deleteFileFromTmp("dumpfile.txt");
+    
+    super.tearDown();
+  }
+
+  /**
+   * Performs a regression test, comparing against previously generated output.
+   */
+  public void testRegression() {
+    performRegressionTest(
+        new TmpFile[]{
+          new TmpFile("dumpfile.txt")
+        });
+  }
+
+  /**
    * 
    * Returns a test suite.
    *
@@ -64,7 +106,43 @@ public class DL4JModelSetupTest
    * @return a suitably configured <code>AbstractActor</code> value
    */
   public AbstractActor getActor() {
-    return new Flow();
+    AbstractArgumentOption    argOption;
+    
+    Flow flow = new Flow();
+    
+    try {
+      List<Actor> actors = new ArrayList<>();
+
+      // Flow.DL4JModelSetup
+      DL4JModelSetup dl4jmodelsetup = new DL4JModelSetup();
+      SimpleMultiLayerNetwork simplemultilayernetwork = new SimpleMultiLayerNetwork();
+      dl4jmodelsetup.setModel(simplemultilayernetwork);
+
+      actors.add(dl4jmodelsetup);
+
+      // Flow.Convert
+      Convert convert = new Convert();
+      DL4JModelToYaml dl4jmodeltoyaml = new DL4JModelToYaml();
+      convert.setConversion(dl4jmodeltoyaml);
+
+      actors.add(convert);
+
+      // Flow.DumpFile
+      DumpFile dumpfile = new DumpFile();
+      argOption = (AbstractArgumentOption) dumpfile.getOptionManager().findByProperty("outputFile");
+      dumpfile.setOutputFile((PlaceholderFile) argOption.valueOf("${TMP}/dumpfile.txt"));
+      actors.add(dumpfile);
+      flow.setActors(actors.toArray(new Actor[0]));
+
+      NullListener nulllistener = new NullListener();
+      flow.setFlowExecutionListener(nulllistener);
+
+    }
+    catch (Exception e) {
+      fail("Failed to set up actor: " + e);
+    }
+    
+    return flow;
   }
 
   /**
@@ -73,7 +151,7 @@ public class DL4JModelSetupTest
    * @param args	ignored
    */
   public static void main(String[] args) {
-    Environment.setEnvironmentClass(Environment.class);
+    Environment.setEnvironmentClass(adams.env.Environment.class);
     runTest(suite());
   }
 }
