37fab94cb52af5c85b53f2e748694090ce38f786
fracpete <fracpete@gmail.com>
2016-05-18 01:34:30 +0000

    added abstract classes

---
+++ adams-dl4j/src/main/java/adams/ml/dl4j/model/AbstractModelConfigurator.java
@@ -0,0 +1,65 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * AbstractModelConfigurator.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, NZ
+ */
+
+package adams.ml.dl4j.model;
+
+import adams.core.option.AbstractOptionHandler;
+import org.deeplearning4j.nn.api.Model;
+
+/**
+ * Ancestor for model configurators.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public abstract class AbstractModelConfigurator
+  extends AbstractOptionHandler
+  implements ModelConfigurator {
+
+  private static final long serialVersionUID = -5049221729823530346L;
+
+  /**
+   * Hook method before configuring the model.
+   * <br>
+   * Default implementation does nothing.
+   *
+   * @return		null if successful, otherwise error message
+   */
+  protected String check() {
+    return null;
+  }
+
+  /**
+   * Configures the actual {@link Model} and returns it.
+   *
+   * @return		the model
+   */
+  protected abstract Model doConfigureModel();
+
+  /**
+   * Configures the {@link Model} and returns it.
+   *
+   * @return		the model
+   */
+  public Model configureModel() {
+    check();
+    return doConfigureModel();
+  }
+}
